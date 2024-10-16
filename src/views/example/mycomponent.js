58f67aca765ad39b1fa1727919adfe3316eadc3b
Le Huy <onebit.huylt@gmail.com>
2024-10-16 20:04:20 +0200

    event

--- src/views/example/mycomponent.js
+++ src/views/example/mycomponent.js
@@ -5,13 +5,31 @@ state = {
     name: 'huy',
     country: 'Sweden'
 }
+handleOnChangeName = (event) => {
+    this.setState({
+        name: event.target.value
+    })
+}
+handleClickButton = () =>{
+    alert('Another hello');
+}
     render() {
-        let name = 'Huy';
-
         return (
             <>
-                <div className="first"> hello my component, my name is {this.state.name}.</div>
+                <div className="first"> 
+                <input value={this.state.name} type="text" 
+                onChange={
+                    (event) => this.handleOnChangeName(event)
+                    } />
+                Hello my name is {this.state.name}.
+                </div>
                 <div className="second">My country is: {this.state.country}</div>
+                <div className="third">
+                    <button onClick={() => {alert('Hello')}}>click me</button>
+                </div>
+                <div className="forth">
+                    <button  onClick={() => this.handleClickButton()}>click me again!</button>
+                </div>
             </>
 
         ) 
