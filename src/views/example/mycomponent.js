98c14851182fe443075d11b5da50ec5f747de351
Le Huy <onebit.huylt@gmail.com>
2024-10-16 20:03:31 +0200

    event

--- src/views/example/mycomponent.js
+++ src/views/example/mycomponent.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class MyComponent extends React.Component{
-
+state = {
+    name: 'huy',
+    country: 'Sweden'
+}
     render() {
+        let name = 'Huy';
+
         return (
-            <div> hello my component.</div>
+            <>
+                <div className="first"> hello my component, my name is {this.state.name}.</div>
+                <div className="second">My country is: {this.state.country}</div>
+            </>
+
         ) 
     }
 
